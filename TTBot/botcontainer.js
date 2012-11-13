18caaf3839770326bdeeeaef35ff34c9b6ee0be7
ratz-n-cask <prakash@asanet.com>
2012-11-13 01:30:23 -0500

    Added colorize log styles

--- TTBot/botcontainer.js
+++ TTBot/botcontainer.js
@@ -1,10 +1,16 @@
+var colorize = require("colorize");
 var BotInfo = require("./botinfo").BotInfo;
 var BasicBot = require("./BasicBot").start;
 
 var BotContainer = function(bot) {
 	this.botinfo = new BotInfo(bot, this);
 	this.basicbot = new BasicBot(bot, this);
+	this.logcolortext = function(text,color) {
 
+		var fulltext = colorize.ansify("#" + color + "[" + text + "]");
+		console.log(fulltext);
+
+	}
 	return this;
 }
 
